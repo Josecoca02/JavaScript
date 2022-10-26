905387d1f845fe870e84b3124f41e86acc7e2023
Josecoca02 <Josecoca0213@gmail.com>
2022-10-26 20:13:28 +0200

    cosas hechas ayer
    dia 25 por la tarde

---
+++ ejercicioMiercoles.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/* Sin utilizar el método sort() de la clase array, se debe generar un array ordenado partiendo de un array desordenado.
+ La restricción obligatoria es que el array Desordenado se leerá una sola vez.
+ Como el objetivo es que practiquen lógica, os planteo estas tres opciones, para que cada uno use la que más le apatece:
+ Opción 1: usando el método splice para ir colocando los elementos en su posición correcta del array Ordenado
+ Opción 2: sin usar el método splice. Solamente se pueden usar push() o unShift().
+ Opción 3: sin usar splice ni unShift. Solamente se puede usar push(). */
+
+var parrafo = new Array("levanto ", "todas", "las", "mañanas", "Me", "a", "7");
+function mostrarNros(arreglo) {
+    arreglo.forEach(palabra => {
+        if (!isNaN(palabra)) {
+            console.log(palabra);
+        }
+    });
+}
+
+
