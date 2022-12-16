c0b18466e3bc3d39146c1777ce549898626e622b
Josecoca02 <Josecoca0213@gmail.com>
2022-12-16 11:14:32 +0100

    a

--- simulacro_diciembre_2022/ejercicio4/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $contrasena = '';
-    $base_datos = 'db_paises';
-
-    $conexion = new Mysqli($servidor, $usuario, $contrasena, $base_datos) 
-        or die("Error en la conexión");
-?>
